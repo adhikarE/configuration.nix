3f81f06869309fbe1c8d9439f3054c51aadeeb5e
adhikari <122161229+adhikarE@users.noreply.github.com>
2025-05-21 13:50:37 +0530

    Update configuration.nix
    clean

--- configuration.nix
+++ configuration.nix
@@ -252,11 +252,6 @@ boot.kernelPatches = [
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-
   networking = {
 
   	# Enable networking
