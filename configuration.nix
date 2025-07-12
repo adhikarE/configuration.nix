116460cb2db9c0cc41ce2ba16d36a6e529446647
adhikari <122161229+adhikarE@users.noreply.github.com>
2025-07-12 16:04:07 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,88 +8,105 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
-  boot.loader.grub = {
-  enable = true;
-  devices = [ "nodev" ];
-  efiSupport = true;
-  useOSProber = true;
-  #version = 2; # Version is 2 by default
-};
+  boot = {
 
-  boot.loader.efi.canTouchEfiVariables = true;
+	loader = {
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+		  # Bootloader.
+		  grub = {
+		  enable = true;
+		  devices = [ "nodev" ];
+		  efiSupport = true;
+		  useOSProber = true;
+		  #version = 2; # Version is 2 by default
+		   };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+		  efi.canTouchEfiVariables = true;
+
+	};
+
+  };
+
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+	  # Select internationalisation properties.
+	  defaultLocale = "en_US.UTF-8";
+
+	  extraLocaleSettings = {
+	    LC_ADDRESS = "en_IN";
+	    LC_IDENTIFICATION = "en_IN";
+	    LC_MEASUREMENT = "en_IN";
+	    LC_MONETARY = "en_IN";
+	    LC_NAME = "en_IN";
+	    LC_NUMERIC = "en_IN";
+	    LC_PAPER = "en_IN";
+	    LC_TELEPHONE = "en_IN";
+	    LC_TIME = "en_IN";
+	  };
+
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services = {
+
+	  # Enable the X11 windowing system.
+	  xserver.enable = true;
+
+	  # Enable the GNOME Desktop Environment.
+	  xserver.displayManager.gdm.enable = true;
+	  xserver.desktopManager.gnome.enable = true;
+
+	  openssh.enable = true;  # This enables the SSH server
+
+	  # Configure keymap in X11
+	  xserver.xkb = {
+	    layout = "us";
+	    variant = "";
+	  };
+
+	  tor = {
+
+	    enable = true;
+	    openFirewall = true;
+
+	  };
+
+	  # Enable CUPS to print documents.
+	  printing.enable = true;
+
+	  pipewire = {
+	    enable = true;
+	    alsa.enable = true;
+	    alsa.support32Bit = true;
+	    pulse.enable = true;
+	    # If you want to use JACK applications, uncomment this
+	    #jack.enable = true;
+
+	    # use the example session manager (no others are packaged yet so this is enabled by default,
+	    # no need to redefine it in your config for now)
+	    #media-session.enable = true;
+	  };
+
+	  # Enable touchpad support (enabled default in most desktopManager).
+	  # xserver.libinput.enable = true;
+
+	  # Enable the OpenSSH daemon.
+	  # openssh.enable = true;
+
+  };
+
 
   # Enable Virtual Box
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = ["adhikari"];
 
-  services.openssh.enable = true;  # This enables the SSH server
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  services.tor = {
-
-    enable = true;
-    openFirewall = true;
-
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.adhikari = {
@@ -101,23 +118,7 @@
     ];
   };
 
-  users.users.dooth = {
-
-    isNormalUser = true;
-    home = "/home/dooth";
-    description = "SSH Client";
-    extraGroups = ["networkmanager" "wheel"];
-    shell = pkgs.bash;
-    openssh.authorizedKeys.keys = [
-    {REDACTED}
-    ];
-
-};
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-boot.kernelPatches = [
+  boot.kernelPatches = [
     {
 
       name = "Rust Support";
@@ -133,22 +134,30 @@ boot.kernelPatches = [
 
   ];
 
-  # Allow Steam To Run
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
+  programs = {
+
+	  # Allow Steam To Run
+	  steam = {
+	    enable = true;
+	    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+	    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+	  };
+
+	  firefox.enable = true;
+
+	  # Some programs need SUID wrappers, can be configured further or are started in user sessions.
+	  # mtr.enable = true;
+	  # gnupg.agent = {
+	  #   enable = true;
+	  #   enableSSHSupport = true;
+	  # };
+
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  security.pki.certificateFiles = [
-
-	# Specify certificate file location
-
-  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -184,7 +193,6 @@ boot.kernelPatches = [
     teams-for-linux
     gns3-gui
     gns3-server
-    ciscoPacketTracer8
     obsidian
     libreoffice-qt6-still
 
@@ -236,31 +244,9 @@ boot.kernelPatches = [
   # Games
   # steamPackages.steam-runtime
 
-
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Device Network Hardening
-
-  services.udev.extraRules = ''
-    # Match RTL-SDR: Realtek Semiconductor Corp. RTL2838 DVB-T
-    SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="2838", MODE="0666", GROUP="plugdev"
-  '';
-
-  boot.blacklistedKernelModules = [ "dvb_usb_rtl28xxu" "rtl2832" "r820t" ];
-
   networking = {
 
   # Enable networking
@@ -275,56 +261,37 @@ boot.kernelPatches = [
     enable = true;
     
     interfaces = {
-	wg0 = {
-	   ips = [ "10.12.0.41/32" ]; # Replace with your assigned client IP
-	   listenPort = 51820;        # Optional; only needed if not default or you are accepting inbound traffic
-	   # mtu = 1360;                # Optional; include if needed
-	   privateKeyFile = "/home/adhikari/.wireguard/priv";
 
-	   peers = [
-		{
-		  publicKey = "nxdlRhfXT3tmn2hOFVCPUsxIPE4A3Ayq3W8V5sCkr2Y=";
-		  endpoint = "114.143.238.94:51820";
-		  allowedIPs = [
-		    "10.12.0.0/24"
-		    "192.168.5.0/24"
-		    "192.168.1.0/24"
-		  ];
-		  persistentKeepalive = 25;
-	  }
-      	];
       };
-    };
   };
 
+  wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # proxy.default = "http://user:password@proxy:port/";
+  # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
   # Optional: Make sure routing is enabled if needed
-  # networking.nat.enable = true;
-  # networking.nat.externalInterface = "eth0";  # Replace with your real external interface
-
-	extraHosts = ''
-	  192.168.5.26 teleport.rudra.local
-	  192.168.5.16 nextcloud.rudra.local
-	  192.168.5.33 gitlab.rudra.local
-	'';
+  # nat.enable = true;
+  # nat.externalInterface = "eth0";  # Replace with your real external interface
 	
-	nftables.enable = true;
+  nftables.enable = true;
 
-  	firewall = {
+  firewall = {
 
-		enable = true;
-		allowPing = false;
-		allowedTCPPorts = [ ];
-		allowedUDPPorts = [ ];
-		logRefusedPackets = true;
+    enable = true;
+    allowPing = false;
+    allowedTCPPorts = [ ];
+    allowedUDPPorts = [ ];
+    logRefusedPackets = true;
 
-  	};
+ };
 
 };
 
   security.pki.certificateFiles = [
 
-	/home/adhikari/.ca-certificates/teleport.rudra.local.crt
-	/home/adhikari/.ca-certificates/nextcloud.rudra.local.crt
+	# Specify certificate file location
 
   ];
 
