5955e11acde25c10bd805fc8f88f0a57c93285a0
adhikari <122161229+adhikarE@users.noreply.github.com>
2026-01-02 20:10:20 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -33,22 +33,6 @@
        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
      '';
 
-	kernelPatches = [
-	    {
-
-	      name = "Rust Support";
-	      patch = null;
-
-	      features = {
-
-		rust = false;	# Make sure the Kernel is above 6.7
-
-	      };
-
-	    }
-
-	  ];
-
 	kernelModules = [ "v4l2loopback"];
 	# kernelModules = [ "8821au" "v4l2loopback"];	# NetCard support comes with kernel 25.11
 
